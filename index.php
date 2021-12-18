de11c65990c21f915374ad557bab2d4c27494c57
stepnayadoch <smile9224@mail.ru>
2021-12-18 16:02:53 +0300

    первое занятие по php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "Hello World!";
+
+echo "<br>";
+
+echo "Hello!";
+
+echo "<br>";
+
+echo 50;
+
+echo "<br>";
+
+echo "<button>Click!</button>";
+
+echo "<br>";
+
+echo date("d.m.Y H:i:s");
+
+echo "<br>";
+
+echo 2 + 3;
+
+echo "<br>";
+
+echo 5 - 1;
+echo "<br>";
+echo 2 * 7;
+echo "<br>";
+echo 10 / 5;
+echo "<br>";
+echo 2 ** 3;
+echo "<br>";
+echo 10 % 3;
